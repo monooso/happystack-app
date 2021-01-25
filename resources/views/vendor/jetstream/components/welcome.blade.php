389f512ca32a25fd0bdcdf4f8e893db2ac6ea8ce
Stephen Lewis <code@stephenlewis.me>
2021-01-25 21:14:00 +0000

    Publish Jetstream views, customise dashboard

---
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -0,0 +1,79 @@
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    <div>
+        <x-jet-application-logo class="block h-12 w-auto" />
+    </div>
+
+    <div class="mt-8 text-2xl">
+        Welcome to your Jetstream application!
+    </div>
+
+    <div class="mt-6 text-gray-500">
+        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
+        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
+        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
+        ecosystem to be a breath of fresh air. We hope you love it.
+    </div>
+</div>
+
+<div class="bg-gray-200 bg-opacity-25">
+    <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Project
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Storage
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Email
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Hosting
+                            </th>
+                            <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Actions</span>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <!-- Odd row -->
+                        <tr class="bg-white">
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                Big Corp Inc.
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">👍</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">⚠️</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">🚨</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Delete</a>
+                            </td>
+                        </tr>
+
+                        <!-- Even row -->
+                        <tr class="bg-gray-50">
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                Small Corp Ltd.
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">👍</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">⚠️</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">🚨</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Delete</a>
+                            </td>
+                        </tr>
+
+                        <!-- More items... -->
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
