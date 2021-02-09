72d2a6e8431823fb2b74fb4d8a80823050176347
Stephen Lewis <code@stephenlewis.me>
2021-02-09 10:10:23 +0000

    Add failing test as todo reminder

---
+++ tests/Unit/Parsers/StatusPageTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit\Parsers;
+
+use Tests\TestCase;
+
+class StatusPageTest extends TestCase
+{
+    /** @test */
+    public function it_needs_tests()
+    {
+        $this->assertTrue(false, 'Implement StatusPage parser tests');
+    }
+}
