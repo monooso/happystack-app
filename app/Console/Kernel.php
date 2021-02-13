6c7558ff54fb0ba2fbc8165b30c1216bc20495dd
Stephen Lewis <code@stephenlewis.me>
2021-02-13 22:15:22 +0000

    Install and configure Telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,19 +12,17 @@ class Kernel extends ConsoleKernel
      *
      * @var array
      */
-    protected $commands = [
-        //
-    ];
+    protected $commands = [];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('telescope:prune')->daily();
     }
 
     /**
