6556641930097d9e979995591d0c56f06c3c9093
Stephen Lewis <code@stephenlewis.me>
2021-02-22 10:56:33 +0000

    Cache Guzzle requests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,22 +14,67 @@ use App\Parsers\Aws as AwsParser;
 use App\Parsers\StatusPage as StatusPageParser;
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\HandlerStack;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Storage\LaravelCacheStorage;
+use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $app = $this->app;
+        $this->registerHttpClient();
+        $this->registerFetchers();
+        $this->registerParsers();
+        $this->registerActions();
+    }
 
-        $app->bind(ClientInterface::class, Client::class);
+    /**
+     * Register the Guzzle ClientInterface implementation
+     *
+     * Cache all API results for 4 minutes, regardless of what the response
+     * headers say. This is mostly because the AWS data is not a proper
+     * API endpoint, and behaves poorly.
+     */
+    private function registerHttpClient()
+    {
+        $this->app->singleton(ClientInterface::class, function () {
+            $cacheStorage = new LaravelCacheStorage(Cache::store());
 
-        $app->bind(CreatesProjects::class, CreateProject::class);
+            $cacheMiddleware = new CacheMiddleware(new GreedyCacheStrategy($cacheStorage, 240));
 
-        $app->bind(AwsFetcherContract::class, AwsFetcher::class);
-        $app->bind(AwsParserContract::class, AwsParser::class);
+            $stack = HandlerStack::create();
+            $stack->push($cacheMiddleware, 'cache');
 
-        $app->bind(StatusPageFetcherContract::class, StatusPageFetcher::class);
-        $app->bind(StatusPageParserContract::class, StatusPageParser::class);
+            return new Client(['handler' => $stack]);
+        });
+    }
+
+    /**
+     * Register component status fetchers with the container
+     */
+    private function registerFetchers()
+    {
+        $this->app->bind(AwsFetcherContract::class, AwsFetcher::class);
+        $this->app->bind(StatusPageFetcherContract::class, StatusPageFetcher::class);
+    }
+
+    /**
+     * Register component status parsers with the container
+     */
+    private function registerParsers()
+    {
+        $this->app->bind(AwsParserContract::class, AwsParser::class);
+        $this->app->bind(StatusPageParserContract::class, StatusPageParser::class);
+    }
+
+    /**
+     * Register actions with the container
+     */
+    private function registerActions()
+    {
+        $this->app->bind(CreatesProjects::class, CreateProject::class);
     }
 }
