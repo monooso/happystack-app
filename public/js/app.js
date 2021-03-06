92da2d51bbb50f81aa8e5db5be0581518e08d757
Stephen Lewis <code@stephenlewis.me>
2021-03-06 17:05:56 +0000

    Regenerate assets

--- public/js/app.js
+++ public/js/app.js
@@ -3825,7 +3825,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__.default({
   broadcaster: 'pusher',
-  key: "01471208aa8d2385ec96",
+  key: "",
   cluster: "eu",
   forceTLS: true
 });
