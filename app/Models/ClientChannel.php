77af31922e228490fb6eefb416867f4a09bd28fd
Stephen Lewis <code@stephenlewis.me>
2021-03-15 13:41:02 +0000

    Add scopes

--- app/Models/ClientChannel.php
+++ app/Models/ClientChannel.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Constants\NotificationChannel;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
 final class ClientChannel extends Model
 {
@@ -17,5 +20,36 @@ final class ClientChannel extends Model
     public function project(): BelongsTo
     {
         return $this->belongsTo(Project::class);
+    }
+
+    /**
+     * Limit query results to those channels that can receive a notification
+     *
+     * We send a maximum of one client notification per 24 hours.
+     *
+     * @param Builder $query
+     *
+     * @return Builder
+     */
+    public function scopeCanReceiveNotification(Builder $query): Builder
+    {
+        return $query
+            ->where(
+                fn (Builder $query) => $query
+                ->whereNull('last_notified_at')
+                ->orWhere('last_notified_at', '<=', Carbon::now()->subDay())
+            );
+    }
+
+    /**
+     * Limit query results to 'mail' channels
+     *
+     * @param Builder $query
+     *
+     * @return Builder
+     */
+    public function scopeToMail(Builder $query): Builder
+    {
+        return $query->where('type', NotificationChannel::MAIL);
     }
 }
