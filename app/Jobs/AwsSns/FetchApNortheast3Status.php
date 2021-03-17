05102260f023dcb9e8a0d3e33a2e890beedb7323
Stephen Lewis <code@stephenlewis.me>
2021-03-17 09:20:14 +0000

    Generate AWS SNS jobs

---
+++ app/Jobs/AwsSns/FetchApNortheast3Status.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs\AwsSns;
+
+use App\Jobs\FetchAwsStatus;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+final class FetchApNortheast3Status extends FetchAwsStatus implements ShouldQueue, ShouldBeUnique
+{
+    protected function getComponentId(): string
+    {
+        return 'sns-ap-northeast-3';
+    }
+}
