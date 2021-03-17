ffa68d631c9e35dcd1f37a731fb868a564967f7f
Stephen Lewis <code@stephenlewis.me>
2021-03-17 09:19:23 +0000

    Add AWS SQS migrations

---
+++ database/migrations/2021_03_17_090020_seed_aws_sqs_service.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+final class SeedAwsSqsService extends Migration
+{
+    public function up()
+    {
+        DB::table('services')->insert([
+            'name'        => 'AWS SQS',
+            'description' => 'Amazon Simple Queue Service',
+            'handle'      => 'aws-sqs',
+            'created_at'  => now(),
+            'updated_at'  => now(),
+        ]);
+    }
+
+    public function down()
+    {
+        DB::table('services')->where('handle', 'aws-sqs')->delete();
+    }
+}
