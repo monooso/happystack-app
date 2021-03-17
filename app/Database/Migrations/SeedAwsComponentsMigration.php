dd6f95050959914a7d86cefa5f269b70e49bd492
Stephen Lewis <code@stephenlewis.me>
2021-03-17 09:40:03 +0000

    Refactor component migrations class

--- app/Database/Migrations/SeedAwsComponentsMigration.php
+++ app/Database/Migrations/SeedAwsComponentsMigration.php
@@ -5,71 +5,9 @@ declare(strict_types=1);
 namespace App\Database\Migrations;
 
 use App\Constants\AwsRegion;
-use Exception;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
-use InvalidArgumentException;
 
-abstract class SeedAwsComponentsMigration extends Migration
+abstract class SeedAwsComponentsMigration extends SeedComponentsMigration
 {
-    /**
-     * Run the migration
-     *
-     * @throws Exception
-     */
-    public function up()
-    {
-        $serviceId = $this->getServiceId();
-
-        if (! $serviceId) {
-            $serviceKey = $this->getServiceKey();
-            throw new Exception("The '${serviceKey}' service does not exist");
-        }
-
-        $now = now();
-
-        foreach ($this->getComponents() as $handle => $name) {
-            DB::table('components')->insert([
-                'service_id' => $serviceId,
-                'name'       => $name,
-                'handle'     => $handle,
-                'created_at' => $now,
-                'updated_at' => $now,
-            ]);
-        }
-    }
-
-    /**
-     * Reverse the migration
-     */
-    public function down()
-    {
-        $serviceId = $this->getServiceId();
-
-        if ($serviceId === null) {
-            return;
-        }
-
-        DB::table('components')
-            ->where('service_id', $serviceId)
-            ->whereIn('handle', array_keys($this->getComponents()))
-            ->delete();
-    }
-
-    /**
-     * Get the service ID from the database
-     *
-     * @return null|int
-     *
-     * @throws InvalidArgumentException
-     */
-    protected function getServiceId(): ?int
-    {
-        return DB::table('services')
-            ->where('handle', $this->getServiceKey())
-            ->value('id');
-    }
-
     /**
      * Get an array of all supported components (services and regions)
      *
@@ -84,13 +22,4 @@ abstract class SeedAwsComponentsMigration extends Migration
 
         return array_combine($keys, array_values($regions));
     }
-
-    /**
-     * Get the service key
-     *
-     * For example, "aws-sns".
-     *
-     * @return string
-     */
-    abstract protected function getServiceKey(): string;
 }
---
+++ database/migrations/2021_02_16_102153_seed_mailgun_components.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Database\Migrations\SeedComponentsMigration;
+
+final class SeedMailgunComponents extends SeedComponentsMigration
+{
+    protected function getComponents(): array
+    {
+        return [
+            'mailgun::api'                      => 'API',
+            'mailgun::control-panel'            => 'Control Panel',
+            'mailgun::email-validation'         => 'Email Validation',
+            'mailgun::events-logs'              => 'Events and Logs',
+            'mailgun::inbound-email-processing' => 'Inbound Email Processing',
+            'mailgun::inbox-placement'          => 'Inbox Placement',
+            'mailgun::outbound-delivery'        => 'Outbound Delivery',
+            'mailgun::smtp'                     => 'SMTP',
+        ];
+    }
+
+    protected function getServiceKey(): string
+    {
+        return 'mailgun';
+    }
+}
