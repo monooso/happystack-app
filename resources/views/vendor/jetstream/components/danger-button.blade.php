53451a476a1240df39fa909329fb5f9a9eac911e
Stephen Lewis <code@stephenlewis.me>
2021-03-24 10:19:02 +0000

    Tidy up UI

--- resources/views/vendor/jetstream/components/danger-button.blade.php
+++ resources/views/vendor/jetstream/components/danger-button.blade.php
@@ -1,3 +1,25 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red active:bg-red-600 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
+<button {{ $attributes->merge([
+    'type' => 'button',
+    'class' => '
+        bg-red-700
+        border
+        border-transparent
+        duration-150
+        ease-in-out
+        font-semibold
+        inline-flex
+        items-center
+        px-6
+        py-3
+        rounded-md
+        text-white
+        transition
+        active:bg-red-900
+        disabled:opacity-25
+        focus:outline-none
+        focus:ring-2
+        focus:ring-offset-2
+        focus:ring-red-300
+        hover:bg-red-800
+    ']) }}
+>{{ $slot }}</button>
--- resources/views/vendor/jetstream/components/section-title.blade.php
+++ resources/views/vendor/jetstream/components/section-title.blade.php
@@ -1,9 +1,6 @@
 <div class="md:col-span-1">
     <div class="px-4 sm:px-0">
-        <h3 class="text-lg font-medium text-gray-900">{{ $title }}</h3>
-
-        <p class="mt-1 text-sm text-gray-600">
-            {{ $description }}
-        </p>
+        <h3 class="font-medium text-gray-700 text-lg">{{ $title }}</h3>
+        <p class="mt-1 text-sm text-gray-700">{{ $description }}</p>
     </div>
 </div>
