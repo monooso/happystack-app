279b96e375c5d60f3eb4e4e4f090077622360ddb
Stephen Lewis <code@stephenlewis.me>
2021-03-24 12:49:14 +0000

    Remove ShouldBroadcast from StatusChanged

--- app/Events/StatusChanged.php
+++ app/Events/StatusChanged.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace App\Events;
 
 use App\Models\Component;
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-final class StatusChanged implements ShouldBroadcast
+final class StatusChanged
 {
     use Dispatchable;
     use SerializesModels;
@@ -25,29 +23,5 @@ final class StatusChanged implements ShouldBroadcast
     public function __construct(Component $component)
     {
         $this->component = $component;
-    }
-
-    /**
-     * Broadcast the event on a component-specific channel
-     *
-     * For example, `component-123`
-     *
-     * @return Channel
-     */
-    public function broadcastOn(): Channel
-    {
-        $channelName = 'component-' . $this->component->id;
-
-        return new Channel($channelName);
-    }
-
-    /**
-     * Broadcast an array of data
-     *
-     * @return array
-     */
-    public function broadcastWith(): array
-    {
-        return ['status' => $this->component->status];
     }
 }
