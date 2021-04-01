8af55bf6964146009d05c18502638c6388462bbb
Stephen Lewis <code@stephenlewis.me>
2021-04-01 11:24:49 +0100

    Create deployment script

--- scripts/deploy-to-production.sh
+++ scripts/deploy-to-production.sh
@@ -1,1 +1,40 @@
 #!bin/sh
+
+is_dirty() {
+    if [ -z "$(git status --porcelain=v1 2>/dev/null)" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+# Check that we are on the correct branch
+if [ "$(git rev-parse --abbrev-ref HEAD)" != 'main' ]; then
+    echo 'You must run this script from the "main" branch. Aborting.'
+    exit 1
+fi
+
+# Abort if there are uncommitted changes
+if is_dirty; then
+    echo 'You have uncommitted changes. Aborting.'
+    exit 1
+fi
+
+# Switch to the production branch, and merge the changes from `main`
+# The `main` branch is our source of truth
+git checkout production
+git merge -s recursive -X theirs --ff --commit main
+
+# Build the production assets
+npm run production
+
+# If the assets have changed, commit them
+if is_dirty; then
+    git commit -am 'Build production assets'
+fi
+
+# DEPLOY ALL THE THINGS!
+git push origin production
+
+echo '⚡️ Pushed to production'
+
